32ab2415493aca55651ae23531f94f4f94216667
huang-menghang <449043209@qq.com>
2017-08-29 12:16:19 +0800

    添加图书,品牌控制器

---
+++ modules/web/controllers/BookController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\modules\web\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `web` module
+ */
+class BookController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $this->layout=false;
+        return $this->render('index');
+    }
+
+    public function actionInfo()
+    {
+        $this->layout=false;
+        return $this->render('info');
+    }
+
+    public function actionSet()
+    {
+        $this->layout=false;
+        return $this->render('set');
+    }
+    public function actionImages()
+    {
+        $this->layout=false;
+        return $this->render('images');
+    }
+}
