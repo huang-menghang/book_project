8fbc0ba5adf57f6191164fc4dbebef3b57f19336
huang-menghang <449043209@qq.com>
2017-08-31 18:00:54 +0800

    新增面板,二维码...界面

---
+++ modules/web/controllers/QrcodeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\modules\web\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `web` module
+ */
+class QrcodeController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $this->layout=false;
+        return $this->render('index');
+    }
+
+    public function actionSet()
+    {
+        $this->layout=false;
+        return $this->render('set');
+    }
+}
