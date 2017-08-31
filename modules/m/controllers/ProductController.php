4ed225d7ba55ae85e016189e7ff5655662f86dd9
huang-menghang <449043209@qq.com>
2017-08-31 18:02:22 +0800

    新增客户首页

--- modules/m/controllers/ProductController.php
+++ modules/m/controllers/ProductController.php
@@ -5,12 +5,19 @@ namespace app\modules\m\controllers;
 use yii\web\Controller;
 
 
-class DefaultController extends Controller
+class ProductController extends Controller
 {
-    // 品牌首页
+    // 商品列表
     public function actionIndex()
     {
         $this->layout=false;
         return $this->render('index');
     }
+
+    public function  actionInfo(){
+        $this->layout=false;
+        return $this->render('info');
+
+    }
+
 }
